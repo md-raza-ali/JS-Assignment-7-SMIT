bd5a5770a9656d0d8b8f3c8c8182d3c11f6ae406
Md-RazaAli <muhammadrazaa263@gmail.com>
2023-10-26 09:58:34 -0700

    First commit

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
-function power(a,b){
-    
+// CHAPTER 38 TO 42
+
+// ANSWER NO: 01
+// function power(a, b) {
+//   let result = 1;
+//   for (let i = 1; i <= b; i++) {
+//     result = result * a;
+//   }
+//   return result;
+// }
+// let a = +prompt("Enter a number..");
+// let b = +prompt("Enter a power..");
+// alert(power(a, b));
+
+// ANSWER NO: 02
+function leapYear(year) {
+  if (
+    leapYear == "2000" ||
+    leapYear == "2004" ||
+    leapYear == "2008" ||
+    leapYear == "2012" ||
+    leapYear == "2016" ||
+    leapYear == "2020"
+  ) {
+    alert("That was the leap year!");
+  } else if (
+    leapYear == "2028" ||
+    leapYear == "2032" ||
+    leapYear == "2036" ||
+    leapYear == "2040" ||
+    leapYear == "2044" ||
+    leapYear == "2048"
+  ) {
+    alert("It will be leap year!");
+  } else {
+    alert("It is not the leap year!");
+  }
 }
+let year = prompt("Enter any Year..");
+alert(leapYear(year));
