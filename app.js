fed92db3b07faf04e036f2c0dfdcbb3e8830e07b
Md-RazaAli <muhammadrazaa263@gmail.com>
2023-10-29 07:07:26 -0700

    Initial commit

--- app.js
+++ app.js
@@ -57,25 +57,60 @@
 // alert("Area of triangle" + area);
 
 // ANSWER NO: 04
-function average(math, phy, compSci){
-     let result = (math + phy + compSci)/3;
-     return result;
-}
-function percentage(obtain, total){
-    let result = (obtain/total) * 100;
-    return result;
-}
-function mainFunction(math, phy, compSci){
-    let ave = average(math, phy, compSci);
-    let obtain = math + phy + compSci;
-    let total = 300;
-    let per = percentage(obtain, total);
-    document.write("Average: " + ave);
-    document.write("<br>Percentage: " + per + "%");
-    
-}
-let math = prompt("Enter marks of Mathematics");
-let phy = prompt("Enter marks of Physics");
-let compSci = prompt("Enter marks of Computer");
-mainFunction(math, phy, compSci);
+// function average(math, phy, compSci) {
+//   let result = (math + phy + compSci) / 3;
+//   return result;
+// }
+// function percentage(obtain, total) {
+//   let result = (obtain / total) * 100;
+//   return result;
+// }
+// function mainFunction(math, phy, compSci) {
+//   let ave = average(math, phy, compSci);
+//   let obtain = math + phy + compSci;
+//   let total = 300;
+//   let per = percentage(obtain, total);
+//   document.write("Average: " + ave);
+//   document.write("<br>Percentage: " + per + "%");
+// }
+// let math = +prompt("Enter marks of Mathematics");
+// let phy = +prompt("Enter marks of Physics");
+// let compSci = +prompt("Enter marks of Computer");
+// mainFunction(math, phy, compSci);
 
+// ANSWER NO: 08
+// function meter(input) {
+//   return input * 1000;
+// }
+// function feet() {
+//   let mtr = meter();
+//   return input * 3.28084;
+// }
+// function inches() {
+//   let mtr = meter();
+//   return input * 39.3701;
+// }
+// function centi() {
+//   let mtr = meter();
+//   return input * 100;
+// }
+// let input = +prompt("Enter distance in Kilometer..");
+// document.write(`Distance in Meter: ${meter(input)}   (m)`);
+// document.write(`<br>Distance in Feet: ${feet(input)}   (ft)`);
+// document.write(`<br>Distance in Inches: ${inches(input)}   (in)`);
+// document.write(`<br>Distance in Centimeter: ${centi(input)}   (cm)`);
+
+
+// ANSWER NO: 09
+// let work = 40;
+// let overTimePAy = 12;
+// let overTime = +prompt("Enter total hours worked..");
+// if(overTime > work){
+//   var overTimeWorked = overTime - work;
+//   var result = overTimeWorked * overTimePAy;
+//   alert(`Overtime Pay Rs: ${result}`);
+// }else{
+//   alert("No overtime hours..");
+// }
+
+// ANSWER NO: 10
